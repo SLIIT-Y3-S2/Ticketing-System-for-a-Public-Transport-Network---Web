ecd5b366cb50894d128648a34c047f8766a88b67
Hansana1999 <chaminduhansana5@gmail.com>
2022-10-24 20:55:33 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+export default class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return ( 
+            <div>
+                <h1>Hello. This is parcel react app</h1>
+            </div>
+        )
+    }
+}
