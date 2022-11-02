9d8097672cd683486d42767a97764df9cd6d6d01
Dishancp <it20012106@my.sliit.lk>
2022-11-02 22:20:27 +0530

    Updated

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -68,7 +68,6 @@ const disablePastMonths = () => {
 
   return (
     <div>
-        <hr style={{marginTop:'60px'}} />
         <div className="pic"></div>
         <div className='form'>
             <center>
